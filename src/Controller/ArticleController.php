8585123e472aea4bb83f1d1cb22fea9009e4067a
ngaswilly77 <ngaswilly77@gmail.com>
2020-07-29 09:44:55 +0100

    install twig and security checker, create show template and add comments in show action in ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController 
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,9 +21,15 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'futur page to show one space article "%s"',
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com'
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', '', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
