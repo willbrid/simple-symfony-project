8d35da64598389bd4ec9eb2d0ea8b80321abd40d
ngaswilly77 <ngaswilly77@gmail.com>
2020-07-31 11:58:12 +0100

    install some packages, create markdown, service, logger trait, update articleController

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,8 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+    Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
